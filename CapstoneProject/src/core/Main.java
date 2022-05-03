ba665498f9aa8f58595172f9a39b0e44dbbad896
Edward Lu <edwardlu.042@gmail.com>
2022-05-03 09:39:12 -0700

    skeleton

---
+++ CapstoneProject/src/core/Main.java
@@ -0,0 +1,15 @@
+package core;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
