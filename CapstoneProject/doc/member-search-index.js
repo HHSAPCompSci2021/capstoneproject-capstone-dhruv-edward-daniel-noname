07ec9d7dae859c615bd49a570136dd2a1e06dc36
Daniel Im <dim023@student.fuhsd.org>
2022-05-06 15:24:58 -0700

    made sum stuff?

---
+++ CapstoneProject/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"screens","c":"MenuScreen","l":"draw()"},{"p":"screens","c":"MenuScreen","l":"MenuScreen(DrawingSurface)","url":"%3Cinit%3E(core.DrawingSurface)"},{"p":"screens","c":"MenuScreen","l":"mousePressed()"},{"p":"screens","c":"MenuScreen","l":"setup()"}]
