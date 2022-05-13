70d0a2280059dd88cbcf64c0a90a67adbbb4e063
Daniel Im <dim023@student.fuhsd.org>
2022-05-13 14:02:05 -0700

    added boolean for touching

--- CapstoneProject/src/sprites/StarShip.java
+++ CapstoneProject/src/sprites/StarShip.java
@@ -2,6 +2,7 @@ package sprites;
 
 import dsharma578.shapes.Rectangle;
 import processing.core.PApplet;
+import java.util.ArrayList;
 
 public class StarShip {
 	
@@ -9,9 +10,24 @@ public class StarShip {
 	private int x,y; 
 	
 	public StarShip() {
-		
+		//75 * 75
+		hitbox = new Rectangle(0, 0, 75, 75);
 	}
-
+	
+	public boolean hitsWall(ArrayList<Rectangle> wallBlocks) 
+	{
+		for(int i=0; i<wallBlocks.size(); i++) 
+		{
+			if(hitbox.intersects(wallBlocks.get(i))) 
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
 	public void draw(PApplet surface) {
 		
 	}
